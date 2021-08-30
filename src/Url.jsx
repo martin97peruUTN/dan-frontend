a2da6de8912bf82d4273a810dfc12d2c6d55d57a
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-08-30 17:13:02 -0300

    fix url pedido

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,5 +1,5 @@
 export const urlBase = 'http://localhost:9011'
 export const userService = urlBase + '/usuarios-srv/api'
 export const productService = urlBase + '/productos-srv/api'
-export const orderService = urlBase + '/pedido-srv/api'
+export const orderService = urlBase + '/pedidos-srv/api'
 //http://localhost:6080/auth/realms/dan-realm/protocol/openid-connect/token
